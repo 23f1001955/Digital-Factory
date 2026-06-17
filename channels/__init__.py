b3158af79b364f187ae31e4148d8d5ecc8ae5077
Kundan Kumar <kundan88240994789@gmail.com>
2026-06-17 10:57:18 +0530

    feat: add GumroadChannel and CHANNEL_REGISTRY to channels package

--- channels/__init__.py
+++ channels/__init__.py
@@ -1,3 +1,8 @@
 from .base import BaseChannel, ProductArtifact, PublishResult, ArtifactFile
+from .gumroad_channel import GumroadChannel
 
-__all__ = ["BaseChannel", "ProductArtifact", "PublishResult", "ArtifactFile"]
+CHANNEL_REGISTRY: dict[str, type[BaseChannel]] = {
+    "gumroad": GumroadChannel,
+}
+
+__all__ = ["BaseChannel", "ProductArtifact", "PublishResult", "ArtifactFile", "GumroadChannel", "CHANNEL_REGISTRY"]
